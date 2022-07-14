5dd2f47273ff39a7b582d57db8adce38b54c1034
气象腊姬 <41361652+WeiYueChina@users.noreply.github.com>
2022-07-14 15:39:27 +0800

    Update ImportExportData.py

--- CommonData/ImportExportData.py
+++ CommonData/ImportExportData.py
@@ -1,4 +1,14 @@
-#读取nc文件
+####读取nc文件
+import xarray as xr
 data= xr.open_dataset('data.nc')
 
-#
+#####读取grib文件
+import xarray as xr
+import cfgrib
+data=xr.open_dataset('data.grib',engine='cfgrib',backend_kwargs={'filter_by_keys':{'dataType': 'cf'}})
+
+#####读取txt文件
+data=np.loadtxt('data.txt').reshape((大小))[可切片]
+
+#####读取npy文件
+data=np.load('data.npy')
