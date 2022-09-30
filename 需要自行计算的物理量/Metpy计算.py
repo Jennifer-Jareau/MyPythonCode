8cc53a529b08c026a7c873a69f61eecbf69e2574
气象腊姬 <41361652+WeiYueChina@users.noreply.github.com>
2022-09-30 14:09:48 +0800

    Update Metpy计算.py

--- 需要自行计算的物理量/Metpy计算.py
+++ 需要自行计算的物理量/Metpy计算.py
@@ -4,3 +4,22 @@
 
 
 # 水汽通量/水汽通量散度
+data=xr.open_dataset(file_name[FF])
+q=data['q']
+u=data['u']
+v=data['v']
+V=(u**2+v**2)**0.5
+Q=q*V/(constants.g*units.second**2/units.meter)   #去单位，单位自己知道就好不知怎么保存，气死
+qu=q*u
+qv=q*v
+
+lon=data['longitude']
+lat=data['latitude']
+times=data['time']
+levels=data['level']
+dx,dy = mpcalc.lat_lon_grid_deltas(lon,lat)
+Qdiv=xr.DataArray(np.zeros((48,23,141,221)), coords=[times,levels,lat,lon], dims=['time', 'level','latitude','longitude'])
+for t in range(len(data['time'])):
+    for l in range(len(data['level'])):
+        Qdiv[t,l]= mpcalc.divergence(u=qu[t,l],v=qv[t,l],dx=dx,dy=dy)
+#完了就按需保存
